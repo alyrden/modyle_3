c25e7fe2973959bb260761309a6527eeb057ad0f
alyrden <f50knox@gmail.com>
2024-09-16 13:50:47 +0300

    3

--- modyle_3.1.py
+++ modyle_3.1.py
@@ -1,1 +1,19 @@
-1 =1
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string: str):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+def is_contains(string: str, list_to_search: list):
+    count_calls()
+    return string.lower() in [item.lower() for item in list_to_search]
+
+print(string_info("Capybara"))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+print(f"Количество вызовов функций: {calls}")
